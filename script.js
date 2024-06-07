39eb8c97186047a637077c82dcfc2ca07a019576
MShambaugh3 <Max.shambaugh3@gmail.com>
2024-06-06 22:57:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById('menu-toggle').addEventListener('click', function() {
+        var navbar = document.getElementById('navbar');
+        navbar.classList.toggle('show');
+    });
+</script>

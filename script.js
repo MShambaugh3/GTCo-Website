caa57fcc1a34f23825d7534a2f3bdd82c3b58649
MShambaugh3 <Max.shambaugh3@gmail.com>
2024-06-10 16:02:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.getElementById('menu-toggle');
-    var navbar = document.getElementById('navbar');
-    var closeIcon = document.getElementById('close');
+ // Email submission functionality
+    async function submitEmail() {
+      const email = document.getElementById('email').value;
+      console.log('Email:', email); // Add this line
+      if (email) {
+        try {
+          const response = await fetch('https://script.google.com/macros/s/AKfycbyRGyiTwny02BePsMdT7fte7EfpJYOxn-jgE2CoEmvA8nVqyfyTZZy1G4pzV065PBfP2A/exec', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email })
+          });
+          const result = await response.json();
+          if (result.status === 'success') {
+            alert('Email successfully submitted!');
+            document.getElementById('email').value = ''; // Clear the email field
+          } else {
+            alert('Submission failed. Please try again.');
+          }
+        } catch (error) {
+          alert('An error occurred. Please try again.');
+        }
+      } else {
+        alert('Please enter a valid email address.');
+      }
+    }
 
-    menuToggle.addEventListener('click', function() {
+    document.addEventListener('DOMContentLoaded', function() {
+      var menuToggle = document.getElementById('menu-toggle');
+      var navbar = document.getElementById('navbar');
+      var closeIcon = document.getElementById('close');
+
+      menuToggle.addEventListener('click', function() {
         navbar.classList.toggle('show');
         closeIcon.style.display = 'initial';
-    });
+      });
 
-    closeIcon.addEventListener('click', function() {
+      closeIcon.addEventListener('click', function() {
         navbar.classList.remove('show');
         closeIcon.style.display = 'none';
+      });
+
+      // Attach the submitEmail function to the button click event
+      document.querySelector('.normal').addEventListener('click', submitEmail);
     });
-});
